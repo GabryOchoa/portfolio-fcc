16c7ef4e01b1d6cbd4e7be3ff1fe6e1393a07f8c
GabryOchoa <AleXyOchoa9@gmail.com>
2023-09-05 12:33:17 -0600

    FCC-11 refactor: separating all components and moving quiz code to its own dir

---
+++ src/quiz/index.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+import { Form } from "./Form";
+
+/* This is the main Quiz component */
+const Quiz = () => {
+  return (
+    <>
+      <Form />
+    </>
+  );
+};
+
+export default Quiz;
