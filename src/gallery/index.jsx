5181b2133dc77b153599a3428822daf06943fcbf
GabryOchoa <AleXyOchoa9@gmail.com>
2023-10-05 12:19:53 -0600

    removed the comment and the state prop the items of the array

--- src/gallery/index.jsx
+++ src/gallery/index.jsx
@@ -6,50 +6,40 @@ const Gallery = () => {
     {
       url: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg",
       alt: "cat sleep",
-      state: true,
     },
     {
       url: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/2.jpg",
       alt: "cat eyes sky blue",
-      state: false,
     },
     {
       url: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/3.jpg",
       alt: "black cat",
-      state: true,
     },
     {
       url: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/4.jpg",
       alt: "while cat sleep",
-      state: false,
     },
     {
       url: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/5.jpg",
       alt: "black cat eyes blue",
-      state: true,
     },
     {
       url: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/6.jpg",
       alt: "two cat",
-      state: false,
     },
     {
       url: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/7.jpg",
       alt: "tucked cat",
-      state: true,
     },
     {
       url: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/8.jpg",
       alt: "orange cat",
-      state: true,
     },
     {
       url: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/9.jpg",
       alt: "black cat and gray cat",
-      state: false,
     },
   ];
-  // const stateArray = galleryArray.filter((value) => value.state);
 
   return (
     <div className="gallery">
